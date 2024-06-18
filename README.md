88f576770d9afc466922a7ef8c8c861c3dc44cb0
KamSaf <bopaf42@gmail.com>
2024-06-18 17:12:42 +0200

    Initial commit, create base express app with postgres connection

---
+++ README.md
@@ -0,0 +1,22 @@
+# Overview
+
+Web application created using TypeScript, React.js and PostgreSQL serving as a tool for viewing simple reports about yearly financial status based on .xml file, which can be found on <a href="https://ekrs.ms.gov.pl/rdf/pd/search_df">this site</a>.
+
+# How to run with Docker Compose:
+
+1. Clone project repository:
+
+        git clone https://github.com/KamSaf/krs_viewer_app.git
+
+
+2. When in project root directory, run command:
+
+        docker compose up
+
+
+# Technologies and tools used:
+
+- Node.js,
+- TypeScript,
+- React.js
+- PostgreSQL,
---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import pg, { Pool } from "pg";
+
+dotenv.config();
+
+const pool: Pool = new pg.Pool({
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
+
+async function dbConnCheck() {
+  try {
+    await pool.connect();
+    console.log("Database connected");
+  } catch (err) {
+    console.log("Database not connected");
+  }
+}
+
+const app: Express = express();
+const port = process.env.SERVER_PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(dbConnCheck());
+});
+
+app.listen(port, () => {
+  console.log(`\n\n⚡️[server]: Server is running at http://localhost:${port}`);
+});
