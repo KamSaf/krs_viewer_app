6c35421488621cd98feaea7eeb97df91f6c65616
KamSaf <bopaf42@gmail.com>
2024-07-02 14:41:32 +0200

    Add type npm script, delete file extensions from imports

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./state/store.ts";
+import { store } from "./state/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 
