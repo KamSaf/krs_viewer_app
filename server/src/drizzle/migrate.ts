2dc99a37f20415f2e744aad0422c908952c15764
KamSaf <bopaf42@gmail.com>
2024-07-19 14:50:37 +0200

    Replace pool and db objects in migrate.ts with imported ones

--- server/src/drizzle/migrate.ts
+++ server/src/drizzle/migrate.ts
@@ -1,18 +1,6 @@
-import { Pool } from "pg";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
-import * as schema from "./schema";
-import { drizzle } from "drizzle-orm/node-postgres";
 import "dotenv/config";
-
-export const pool: Pool = new Pool({
-  host: process.env.POSTGRES_HOST as string,
-  port: parseInt(process.env.POSTGRES_PORT as string),
-  user: process.env.POSTGRES_USER as string,
-  password: process.env.POSTGRES_PASSWORD as string,
-  database: process.env.POSTGRES_DB as string,
-});
-
-const db = drizzle(pool, { schema });
+import { db, pool } from "../db";
 
 async function runMigrations() {
   console.log("### Running migrations... ###");
