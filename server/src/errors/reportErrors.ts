5bb44a09dd25558abc44f593a0fb0ded7f3aa3fc
KamSaf <bopaf42@gmail.com>
2024-08-13 14:05:26 +0200

    Move Errorbase class to separate file

--- server/src/errors/reportErrors.ts
+++ server/src/errors/reportErrors.ts
@@ -1,16 +1,4 @@
-abstract class ErrorBase extends Error {
-  public name: string;
-  public message: string;
-  public cause: Error;
-
-  constructor(message: string, cause: Error) {
-    super(message);
-    this.name = this.constructor.name;
-    this.message = message;
-    this.cause = cause;
-  }
-}
-
+import ErrorBase from "./errorBase";
 export class FileReadingError extends ErrorBase {
   constructor(cause: Error) {
     super("Error occured while trying to read report file.", cause);
